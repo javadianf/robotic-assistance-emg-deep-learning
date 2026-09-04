888bec0def1e526cd02ee6986a0724cb55f04a21
javadianf <165384260+javadianf@users.noreply.github.com>
2026-09-04 12:27:32 +0200

    Remove leading blank line in plot_predictions.py
    Remove unnecessary blank line at the beginning of the file.

--- plot_predictions.py
+++ plot_predictions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (c) 2026 Javadian. All rights reserved.
-
 """
 Diagnostic plots from an exported predictions file.
 
